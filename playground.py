61644a91ca69fdb688a1e6874c3bab83e0b3c76a
danish2562022 <danish2562022@gmail.com>
2022-10-09 00:22:53 +0200

    Removed unwanted files

--- playground.py
+++
@@ -1,5 +0,0 @@
-import yaml
-with open('config.yaml') as f:
-    config = yaml.load(f, Loader = yaml.FullLoader) 
-x =".".join(config['input_files']['models'].split('/')[0:-1])+"."+config['input_files']['models'].split("/")[-1].split(".")[0] + " import *"
-print(x.split(" ")[-3].split(".")[-1])
